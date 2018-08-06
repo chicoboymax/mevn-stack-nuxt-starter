d0d861c1d2716aad1023fd3292b2252d66aa8111
John Doe <john@sample.com>
2018-08-06 13:49:23 -0400

    Added vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,65 +1,40 @@
-const pkg = require('./package')
 
 module.exports = {
-  mode: 'universal',
-
   /*
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'nuxt-vuetify',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: 'Nuxt.js + Vuetify.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
     ]
   },
-
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#FFFFFF' },
-
-  /*
-  ** Global CSS
-  */
+  plugins: ['~/plugins/vuetify.js'],
   css: [
-  ],
-
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios',
-    // Doc: https://bootstrap-vue.js.org/docs/
-    'bootstrap-vue/nuxt'
+    '~/assets/style/app.styl'
   ],
   /*
-  ** Axios module configuration
+  ** Customize the progress bar color
   */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-  },
-
+  loading: { color: '#3B8070' },
   /*
   ** Build configuration
   */
   build: {
+    vendor: [
+      '~/plugins/vuetify.js'
+    ],
+    extractCSS: true,
     /*
-    ** You can extend webpack config here
+    ** Run ESLint on save
     */
-    extend(config, ctx) {
-      // Run ESLint on save
+    extend (config, ctx) {
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
