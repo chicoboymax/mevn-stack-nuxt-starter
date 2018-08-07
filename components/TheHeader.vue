8f265ee3f5024209044aae8fcc12fc415ffca07e
John Doe <john@sample.com>
2018-08-07 09:28:46 -0400

    drawer

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,36 +1,48 @@
 <template>
-  <v-toolbar app>
-    <v-toolbar-side-icon class="hidden-sm-and-up">
+  <div>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
       <v-list>
         <v-list-tile
+          router
+          :to="item.to"
+          :key="i"
+          v-for="item in menuItems"
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar>
+      <v-toolbar-title>
+        <v-toolbar-side-icon @click="drawer = !drawer"
+                             class="hidden-sm-and-up"></v-toolbar-side-icon>
+        <router-link to="/" tag="span" style="cursor: pointer">
+          {{ appTitle }}
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+          flat
           v-for="item in menuItems"
           :key="item.title"
           :to="item.path">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-
-    </v-toolbar-side-icon>
-    <v-toolbar-title>
-      <router-link to="/" tag="span" style="cursor: pointer">
-        {{ appTitle }}
-      </router-link>
-    </v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-xs-only">
-      <v-btn
-        flat
-        v-for="item in menuItems"
-        :key="item.title"
-        :to="item.path">
-        <v-icon left light>{{ item.icon }}</v-icon>
-        {{ item.title }}
-      </v-btn>
-    </v-toolbar-items>
-  </v-toolbar>
+          <v-icon left light>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
 </template>
 
 <script>
@@ -39,6 +51,7 @@
     data () {
       return {
         appTitle: 'MEVN stack starter',
+        drawer: false,
         menuItems: [
           {title: 'Home', path: '/', icon: 'home'},
           {title: 'About', path: '/about', icon: 'group'},
