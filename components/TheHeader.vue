a9b5683dcd958c82e7cc0723dfb3bb9e6e3820d8
John Doe <john@sample.com>
2018-08-07 09:17:45 -0400

    Side icon

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,6 +1,19 @@
 <template>
   <v-toolbar app>
-    <v-toolbar-side-icon class="hidden-sm-and-up"></v-toolbar-side-icon>
+    <v-toolbar-side-icon class="hidden-sm-and-up">
+      <v-list>
+        <v-list-tile
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
+    </v-toolbar-side-icon>
     <v-toolbar-title>
       <router-link to="/" tag="span" style="cursor: pointer">
         {{ appTitle }}
