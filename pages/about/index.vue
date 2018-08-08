a8dc3d3736b9415fb43ad12bc6bd9c46af86417a
John Doe <john@sample.com>
2018-08-08 08:13:59 -0400

    Added about and contact pages

---
+++ pages/about/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container fluid>
+    <v-layout row wrap>
+
+      <v-flex xs12 sm6 offset-sm3 mt-3>
+        <v-card>
+          <v-card-title class="headline">Welcome to the MEVN starter template</v-card-title>
+          <v-card-text>
+            <p>Lorem ipsum dolor sit amet, mel alii iusto civibus ne, labore consequuntur an his. </p>
+            <p>Cum ea omnis elitr accumsan, pro eu assum ubique nostro, nec at epicurei oportere. </p>
+            <p>Ei facer malorum complectitur vix, ex vim labores placerat. Eam in blandit  </p>
+            <p>ullamcorper delicatissimi, usu ne suavitate consetetur. Tota expetendis ex eum, omni </p>
+            <p>nullam ut vix, nonumy nostro salutatus eos et. Fugit pertinax ea vix, eu pri etiam </p>
+            <div class="text-xs-right">
+              <em><small>&mdash; Maxime Drouin</small></em>
+            </div>
+
+          </v-card-text>
+
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {}
+</script>
---
+++ pages/contact/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs12 class="text-xs-center" mt-5>
+        <h1>Contact Us</h1>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3 mt-3>
+        <form>
+          <v-text-field
+            v-model="name"
+            :error-messages="nameErrors"
+            :counter="10"
+            label="Name"
+            required
+            @input="$v.name.$touch()"
+            @blur="$v.name.$touch()"
+          ></v-text-field>
+          <v-text-field
+            v-model="email"
+            :error-messages="emailErrors"
+            label="E-mail"
+            required
+            @input="$v.email.$touch()"
+            @blur="$v.email.$touch()"
+          ></v-text-field>
+          <v-textarea
+            v-model="message"
+            label="Message"
+            auto-grow
+            @input="$v.message.$touch()"
+            @blur="$v.message.$touch()"
+          ></v-textarea>
+
+
+          <v-btn @click="submit">submit</v-btn>
+          <v-btn @click="clear">clear</v-btn>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {}
+</script>
