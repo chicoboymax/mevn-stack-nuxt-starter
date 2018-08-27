5d405be50ba01dd7b1d3da22ddd892da04985fa7
John Doe <john@sample.com>
2018-08-27 12:33:09 -0400

    Added credit to user account

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -2,18 +2,18 @@ const keys = require("../config/keys")
 const stripe = require("stripe")(keys.stripeSecretKey)
 
 module.exports = app => {
-  app.post("/api/stripe", (req, res) => {
-    console.log(req.body)
-
-    stripe.charges.create({
+  app.post("/api/stripe", async (req, res) => {
+    const charge = await stripe.charges.create({
       amount: 500,
       currency: "usd",
       description: "$5 for 5 credits",
       source: req.body.id
     })
+    console.log(charge)
 
-    res.json({
-      ok: true
-    })
+    req.user.credits += 5
+
+    const user = await req.user.save()
+    res.send(user)
   })
 }
