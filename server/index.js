b95777cffd7eb6e45003f76a28b1a7bfc5845579
John Doe <john@sample.com>
2018-08-29 12:43:18 -0400

    heroku config

--- server/index.js
+++ server/index.js
@@ -29,6 +29,15 @@ app.use(passport.session())
 require("./routes/authRoutes")(app)
 require("./routes/billingRoutes")(app)
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
+
+  const path = require("path")
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
 app.set("port", port)
 
 // Import and Set Nuxt.js options
