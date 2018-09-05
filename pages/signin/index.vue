9b7011e3955dcab88dbfcd94f2893772d9931530
John Doe <john@sample.com>
2018-09-05 07:32:38 -0400

    css changes on social buttons

--- pages/signin/index.vue
+++ pages/signin/index.vue
@@ -1,39 +1,41 @@
 <template>
-  <v-container grid-list-xl>
-    <v-layout align-center justify-center justify-end fill-height>
-      <v-flex xs2 text-xs-center>
-        <v-btn
-          color="#3B5998"
-          class="white--text social-button"
-          large
-          href="/auth/facebook"
-        >
-          <v-icon class="fa fa-facebook" left/>
-          Sign in with Facebook
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3 mt-3>
+        <v-layout column>
+          <v-btn
+            color="#3B5998"
+            class="white--text social-button"
+            large
+            href="/auth/facebook"
+          >
+            <v-icon class="fa fa-facebook" left/>
+            Sign in with Facebook
 
-        </v-btn>
+          </v-btn>
 
-        <v-btn
-          color="#1DA1F2"
-          class="white--text social-button"
-          large
-          href="/auth/twitter"
-        >
-          <v-icon class="fa fa-twitter" left/>
-          Sign in with Twitter
+          <v-btn
+            color="#1DA1F2"
+            class="white--text social-button"
+            large
+            href="/auth/twitter"
+          >
+            <v-icon class="fa fa-twitter" left/>
+            Sign in with Twitter
 
-        </v-btn>
+          </v-btn>
 
-        <v-btn
-          color="#DB4437"
-          class="white--text social-button"
-          large
-          href="/auth/google"
-        >
-          <v-icon class="fa fa-google" left/>
-          Sign in with Google
+          <v-btn
+            color="#DB4437"
+            class="white--text social-button"
+            large
+            href="/auth/google"
+          >
+            <v-icon class="fa fa-google" left/>
+            Sign in with Google
 
-        </v-btn>
+          </v-btn>
+        </v-layout>
       </v-flex>
 
     </v-layout>
